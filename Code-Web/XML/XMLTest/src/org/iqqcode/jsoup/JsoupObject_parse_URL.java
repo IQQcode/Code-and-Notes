8b0452b38b2cdf2adf225fc49310c44e12b2806c
Mr.Q <2726109782@qq.com>
2020-01-21 16:16:19 +0800

    Jsoup_parse

--- Code-Web/XML/XMLTest/src/org/iqqcode/jsoup/JsoupObject_parse_URL.java
+++ Code-Web/XML/XMLTest/src/org/iqqcode/jsoup/JsoupObject_parse_URL.java
@@ -14,8 +14,8 @@ import java.net.URL;
 public class JsoupObject_parse_URL {
     public static void main(String[] args) throws IOException {
         //1.获取Document对象,根据xml对象获取
-        //获取student.xml的path
-        String path = JsoupDemo.class.getClassLoader().getResource("user.xml").getPath();
+        //获取student.xml的path(类名)
+        String path = JsoupObject_parse_URL.class.getClassLoader().getResource("user.xml").getPath();
         //2.解析xml文档,将文档加载如内存,获取DOM树
         //parse(URL url,int timeoutMillins):通过网络路径获取指定的html或xml的文档对象
         URL url = new URL("https://baike.sogou.com/v63211637.htm?fromTitle=jsoup");
